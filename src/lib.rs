6395bc31e244f3781e18b05b0399004b82f59cd3
Joseph Diza <josephm.diza@gmail.com>
2022-04-14 00:16:17 -0700

    Add format_rendered unit test

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,13 @@ pub mod tmpl;
 pub mod keyboard;
 pub mod parse;
 pub mod linux;
+
+#[cfg(test)]
+pub mod tests {
+    use crate::tmpl::format_rendered;
+    #[test]
+    pub fn format_rendered_should_not_be_none() {
+        let result = format_rendered("MyTitle", "MyRenderedContent");
+        assert_ne!(Some(result), None);
+    }
+}
